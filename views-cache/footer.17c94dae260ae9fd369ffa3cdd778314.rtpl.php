36b8fcfcb882f911622f693f24531f32a1cb9594
João Araujo <araujo_joaol@yahoo.com.br>
2022-09-14 22:51:57 -0300

    chore: header and footer update

--- views-cache/footer.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/footer.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -59,7 +59,7 @@
 		<div class="row">
 			<div class="col-md-8">
 				<div class="copyright">
-					<p>&copy; 2017 Hcode Treinamentos. <a href="http://www.hcode.com.br" target="_blank">hcode.com.br</a></p>
+					<p>&copy; 2022 joaocaraujo store. <a href="https://github.com/joaocaraujo" target="_blank">click here</a></p>
 				</div>
 			</div>
 			
