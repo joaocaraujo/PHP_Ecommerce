487b915420e29543cb29e16af5808ec423a70e96
João Araujo <araujo_joaol@yahoo.com.br>
2022-09-18 19:16:54 -0300

    feat: CRUD routes

--- index.php
+++ index.php
@@ -57,6 +57,33 @@ $app->get('/admin/logout', function() {
 	exit;
 });
 
+$app->get('/admin/users', function() {
+
+	User::verifyLogin();
+	
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users");
+});
+
+$app->get('/admin/users/create', function() {
+
+	User::verifyLogin();
+	
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser', function($iduser) {
+
+	User::verifyLogin();
+	
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users-update");
+});
+
 
 $app->run();
 
