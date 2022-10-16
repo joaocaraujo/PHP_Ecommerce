237fa0c3f5396939e8a0eefd18c70b62af8cf83b
João Araujo <araujo_joaol@yahoo.com.br>
2022-10-16 09:55:10 -0300

    feat: CRUD products in admin page

---
+++ site.php
@@ -0,0 +1,23 @@
+<?php
+
+use \Hcode\Page;
+use \Hcode\Model\Product;
+use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
+use \Hcode\Model\Address;
+use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
+
+$app->get('/', function() {
+    
+	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+});
+
+
+
+
+?>
