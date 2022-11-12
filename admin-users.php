7ebdf6c0d2c52f8f1e29bb3273814b7927344bd4
João Araujo <araujo_joaol@yahoo.com.br>
2022-11-12 12:35:51 -0300

    feat: Pagination and Search in Users at Admin Page

--- admin-users.php
+++ admin-users.php
@@ -2,17 +2,46 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
-$app->get('/admin/users', function() {
+$app->get("/admin/users", function() {
 
-User::verifyLogin();
+	User::verifyLogin();
 
-$users = User::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-$page = new Hcode\PageAdmin();
+	if ($search != '') {
 
-$page->setTpl("users", array(
-    "users" => $users
-));
+		$pagination = User::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = User::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/users?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	));
+
 });
 
 $app->get('/admin/users/create', function() {
